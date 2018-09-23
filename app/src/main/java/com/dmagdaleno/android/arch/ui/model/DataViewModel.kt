17a14c3ca474f75d35a355829295aa3bc5e8f29d
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-09-22 23:02:02 -0300

    add livedata wrapper to the viewmodel attribute

--- app/src/main/java/com/dmagdaleno/android/arch/ui/model/DataViewModel.kt
+++ app/src/main/java/com/dmagdaleno/android/arch/ui/model/DataViewModel.kt
@@ -1,21 +1,25 @@
 package com.dmagdaleno.android.arch.ui.model
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.util.Log
 import com.dmagdaleno.android.arch.model.Data
 
-class DataViewModel(): ViewModel() {
+class DataViewModel: ViewModel() {
 
-    private val datas: List<Data>
+    private val dataList: LiveData<List<Data>>
 
     init {
-        Log.i("ViewModel", "building model")
+        Log.d("ViewModel", "building model")
 
         val data1 = Data("val1", "val2", "val3")
         val data2 = Data("val1", "val2", "val3")
         val data3 = Data("val1", "val2", "val3")
-        datas = listOf(data1, data2, data3)
+
+        dataList = MutableLiveData<List<Data>>()
+        dataList.value = listOf(data1, data2, data3)
     }
 
-    fun getDataList() = datas
+    fun getDataList() = dataList
 }
