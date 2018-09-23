f4427b5ebbef4dc509746c83fbcdaf8d95841b1b
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-09-22 22:06:53 -0300

    simple model with some attributes

---
+++ app/src/main/java/com/dmagdaleno/android/arch/model/Data.kt
@@ -0,0 +1,7 @@
+package com.dmagdaleno.android.arch.model
+
+data class Data(
+        val attr1: String = "undefined",
+        val attr2: String = "undefined",
+        val attr3: String = "undefined"
+)
