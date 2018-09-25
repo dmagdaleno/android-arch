ab09f84c6db38a2882ff567140c16853736115cb
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-09-25 00:18:51 -0300

    add entity annotation

--- app/src/main/java/com/dmagdaleno/android/arch/model/Data.kt
+++ app/src/main/java/com/dmagdaleno/android/arch/model/Data.kt
@@ -1,7 +1,13 @@
 package com.dmagdaleno.android.arch.model
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity
 data class Data(
-        val attr1: String = "undefined",
-        val attr2: String = "undefined",
-        val attr3: String = "undefined"
+        @PrimaryKey(autoGenerate = true)
+        var id: Int = 0,
+        var attr1: String = "undefined",
+        var attr2: String = "undefined",
+        var attr3: String = "undefined"
 )
