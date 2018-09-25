5fed043b34f3f50550b86978963e38b016393c00
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-09-25 00:20:40 -0300

    get data from database as livedata

--- app/src/main/java/com/dmagdaleno/android/arch/ui/model/DataViewModel.kt
+++ app/src/main/java/com/dmagdaleno/android/arch/ui/model/DataViewModel.kt
@@ -1,24 +1,24 @@
 package com.dmagdaleno.android.arch.ui.model
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
+import android.arch.persistence.room.Room
 import android.util.Log
+import com.dmagdaleno.android.arch.db.AppDatabase
 import com.dmagdaleno.android.arch.model.Data
 
-class DataViewModel: ViewModel() {
+class DataViewModel(application: Application) : AndroidViewModel(application) {
 
     private val dataList: LiveData<List<Data>>
 
     init {
         Log.d("ViewModel", "building model")
+        val database = Room
+                .databaseBuilder(application, AppDatabase::class.java, "app.db")
+                .build()
 
-        val data1 = Data("val1", "val2", "val3")
-        val data2 = Data("val1", "val2", "val3")
-        val data3 = Data("val1", "val2", "val3")
-
-        dataList = MutableLiveData<List<Data>>()
-        dataList.value = listOf(data1, data2, data3)
+        dataList = database.getDataDao().getAll()
     }
 
     fun getDataList() = dataList
