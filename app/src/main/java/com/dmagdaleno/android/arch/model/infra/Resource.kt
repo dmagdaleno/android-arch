3a2a60ef829743336757f530ae4dfe87ac85be68
Diego Magdaleno <diegommagdaleno@gmail.com>
2018-09-29 14:32:13 -0300

    add resource class

---
+++ app/src/main/java/com/dmagdaleno/android/arch/model/infra/Resource.kt
@@ -0,0 +1,17 @@
+package com.dmagdaleno.android.arch.model.infra
+
+data class Resource<ResultType>(
+        var status: Status,
+        var data: ResultType? = null,
+        var errorMessage: String? = null) {
+
+    companion object {
+        fun <ResultType> success(data: ResultType): Resource<ResultType> =
+                Resource(Status.SUCCESS, data)
+
+        fun <ResultType> loading(): Resource<ResultType> = Resource(Status.LOADING)
+
+        fun <ResultType> error(message: String?): Resource<ResultType> =
+                Resource(Status.ERROR, errorMessage = message)
+    }
+}
